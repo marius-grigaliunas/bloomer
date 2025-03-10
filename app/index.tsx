9ae08d2b6be3e232771d82b87f2be4a6d9ca49d6
Marius Grigaliunas <cia.marius.grigaliunas@gmail.com>
2025-03-11 00:03:53 +0100

    added natiwind

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,10 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 justify-center items-center" >
+      <Text className="text-green-700 font-semibold">Bloomer</Text>
+      <Text className=""> by </Text>
+      <Text className="text-red-700 font-bold">Marius Grigaliunas</Text> 
     </View>
   );
 }
---
+++ nativewind-env.d.ts
@@ -0,0 +1,1 @@
+/// <reference types="nativewin/types" />
