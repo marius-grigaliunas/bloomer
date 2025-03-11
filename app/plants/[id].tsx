b207ed12e702bc45103f3bdde1e71c16c5d20a1f
Marius Grigaliunas <cia.marius.grigaliunas@gmail.com>
2025-03-11 09:17:50 +0100

    started the routing layout

---
+++ app/plants/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PlantDetails = () => {
+  return (
+    <View>
+      <Text>PlantDetails</Text>
+    </View>
+  )
+}
+
+export default PlantDetails
