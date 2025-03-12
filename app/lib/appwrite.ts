a788cf569587dd399795792e1a0f6b3a0b673616
Marius Grigaliunas <cia.marius.grigaliunas@gmail.com>
2025-03-12 11:08:15 +0100

    started the auth

---
+++ app/lib/appwrite.ts
@@ -0,0 +1,27 @@
+import { Account, Avatars, Client } from "react-native-appwrite"
+import * as Linking from 'expo-linking'
+
+export const config = {
+    platform: 'com.margri.restate',
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+}
+
+export const client = new Client();
+
+client
+    .setEndpoint(config.endpoint!)
+    .setProject(config.projectId!)
+    .setPlatform(config.platform!)
+
+export const avatar = new Avatars(client)
+export const accound = new Account(client)
+
+export async function login () {
+    try {
+        const redirectUri = Linking.createURL("/");
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
