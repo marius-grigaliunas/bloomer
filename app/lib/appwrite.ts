ebb560571da75977ff112b7ca2f897da354a10d0
Marius Grigaliunas <cia.marius.grigaliunas@gmail.com>
2025-03-13 01:00:29 +0100

    added auth functions

--- app/lib/appwrite.ts
+++ app/lib/appwrite.ts
@@ -1,5 +1,7 @@
-import { Account, Avatars, Client } from "react-native-appwrite"
+import { Account, Avatars, Client, OAuthProvider } from "react-native-appwrite"
 import * as Linking from 'expo-linking'
+import * as WebBrowser from 'expo-web-browser'
+
 
 export const config = {
     platform: 'com.margri.restate',
@@ -15,13 +17,66 @@ client
     .setPlatform(config.platform!)
 
 export const avatar = new Avatars(client)
-export const accound = new Account(client)
+export const account = new Account(client)
 
 export async function login () {
     try {
         const redirectUri = Linking.createURL("/");
+
+        const response = await account.createOAuth2Token(OAuthProvider.Google, redirectUri); 
+
+        if(!response) throw new Error("Failed to login"); 
+
+        const browserResult = await WebBrowser.openAuthSessionAsync(
+            response.toString(), 
+            redirectUri
+        );
+
+        if(browserResult.type !== "success") throw new Error("Failed to login");
+        
+        const url = new URL(browserResult.url);
+
+        const secret = url.searchParams.get('secret')?.toString();
+        const userId = url.searchParams.get('userId')?.toString();
+
+        if(!secret || !userId) throw new Error("Failed to get secret/userId");
+
+        const session = await account.createSession(userId, secret);
+        if(!session) throw new Error("Fao;ed to login");
+
+        return true;
+
     } catch (error) {
         console.error(error);
         return false;
+    }
+}
+
+export async function logout () {
+    try {
+        await account.deleteSession('current');
+        return true;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+
+export async function getUser() {
+    try {
+        const response = await account.get()
+
+        if(response.$id) {
+            const userAvatar = avatar.getInitials(response.name);
+
+            return {
+                ...response,
+                avatar: userAvatar.toString(),
+            }
+        }
+
+    } catch (error) {
+        console.error(error)
+        return null;
     }
 }
