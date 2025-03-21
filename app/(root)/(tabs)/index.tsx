93e5508a0fc594bb3e9db43ea668c6d4eb61ef69
Marius Grigaliunas <cia.marius.grigaliunas@gmail.com>
2025-03-21 21:42:38 +0100

    need a redesign

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -5,10 +5,7 @@ import SignIn from '../../sign-in';
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center bg-background " >
-      <Text className="text-green-700 font-semibold text-3xl">Bloomer</Text>
-      <Text className="text-xl"> by </Text>
-      <Text className="text-red-700 font-bold text-3xl">Marius Grigaliunas</Text> 
-      <Link className="mt-10 text-3xl text-white" href={"/sign-in"}>SignIn</Link>
+      
     </View>
   );
 }
