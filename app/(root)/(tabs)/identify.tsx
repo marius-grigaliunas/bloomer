69af0668294967f266a8b7a40d4480176ddb456e
Marius Grigaliunas <cia.marius.grigaliunas@gmail.com>
2025-04-02 23:35:36 +0200

    added a searchbar, installed expo-camera package

--- app/(root)/(tabs)/identify.tsx
+++ app/(root)/(tabs)/identify.tsx
@@ -1,10 +1,23 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import SearchBar from '@/components/SearchBar'
 
 const identify = () => {
+
+  const [searchQuery, setSearchQuery] = useState("");
+
   return (
-    <View className='flex justify-center items-center w-full h-full'>
-      <Text className='text-3xl'>This is for the plant identification/search section</Text>
+    <View className='flex justify-center items-center w-full h-full bg-background-primary'>
+      <SearchBar
+        placeholder='Search for your plant'
+        value={searchQuery}
+        onChangeText={(text: string) => setSearchQuery(text)}
+      />
+      <Text
+        className='text-3xl text-text-primary'
+      >
+        {searchQuery}
+      </Text>
     </View>
   )
 }
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -4,21 +4,23 @@ import { icons } from '@/constants/icons'
 import colors from '@/constants/colors'
 
 interface SearchBarProps {
-    pressed?: () => {};
+    pressed?: () => void;
     placeholder: string;
+    value: string;
+    onChangeText: (text: string) => void; 
 }
 
-const SearchBar = ({pressed, placeholder}: SearchBarProps) => {
+const SearchBar = ({pressed, placeholder, value, onChangeText}: SearchBarProps) => {
   return (
-    <View className='flex-row items-center bg-background-surface rounded-full  px-5 py-4'>
+    <View className='flex-row items-center bg-background-surface rounded-full px-5 py-4 shadow-2xl shadow-secondary-deep'>
       <Image source={icons.search} className='size-5' resizeMode='contain' tintColor={colors.secondary.medium} />
       <TextInput 
         onPress={pressed}
         placeholder={placeholder}
-        value=''
-        onChangeText={() => {}}
+        value={value}
+        onChangeText={onChangeText}
         placeholderTextColor={colors.secondary.medium}
-        className='flex-1 ml-2 text-white'
+        className='flex-1 ml-2 text-secondary-medium'
       />
     </View>
   )
