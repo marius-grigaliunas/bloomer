6d322e83189f2cea5d9a374215caab4d8a3f9ef5
Marius Grigaliunas <cia.marius.grigaliunas@gmail.com>
2025-04-03 21:33:20 +0200

    added a camera view

--- app/(root)/(tabs)/identify.tsx
+++ app/(root)/(tabs)/identify.tsx
@@ -1,23 +1,62 @@
-import { View, Text } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, Button, Pressable } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
 import SearchBar from '@/components/SearchBar'
+import * as ExpoCamera from 'expo-camera'
+import { Permission } from 'react-native-appwrite'
+
 
 const identify = () => {
 
   const [searchQuery, setSearchQuery] = useState("");
+  const [cameraPermission, requestCameraPermission] = ExpoCamera.useCameraPermissions();
+  const ref = useRef<ExpoCamera.CameraView>(null)
+  const [uri, setUri] = useState<string | null>(null);
+
+  const takePicture = async () => {
+    const photo = await ref.current?.takePictureAsync();
+    setUri(photo?.uri ?? null);
+  }
+
+
+  if(!cameraPermission) {
+    return null;
+  }
+
+  if(!cameraPermission?.granted) {
+    return (
+      <View >
+        <Text className='text-3xl text-text-primary'>We need your permission to show the camera</Text>
+        <Button onPress={requestCameraPermission} title="grant permission" />
+      </View>
+    );
+  }
 
   return (
-    <View className='flex justify-center items-center w-full h-full bg-background-primary'>
-      <SearchBar
-        placeholder='Search for your plant'
-        value={searchQuery}
-        onChangeText={(text: string) => setSearchQuery(text)}
-      />
-      <Text
-        className='text-3xl text-text-primary'
+    <View className='flex justify-start items-center w-full h-full bg-background-primary'>
+      <ExpoCamera.CameraView 
+        className='flex' 
+        facing={'back'}
+        ref={ref}
+        mode={'picture'}
+        mute={true}
+        responsiveOrientationWhenOrientationLocked
       >
-        {searchQuery}
-      </Text>
+        <View className='flex bg-transparent h-4/6 w-screen rounded-full'>
+          <Pressable onPress={takePicture}/>
+        </View>
+      </ExpoCamera.CameraView>
+      <View className='flex justify-center items-center w-screen mt-5'>
+        <SearchBar
+          placeholder='Search for your plant'
+          value={searchQuery}
+          onChangeText={(text: string) => setSearchQuery(text)}
+        />
+        <Text
+          className='text-3xl text-text-primary'
+        >
+          {searchQuery}
+        </Text>
+      </View>
     </View>
   )
 }
