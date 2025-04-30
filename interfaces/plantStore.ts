6949a0bb90f140d482db5dc319e01713dabedd5a
Marius Grigaliūnas <cia.marius.grigaliunas@gmail.com>
2025-04-30 12:54:02 +0200

    short work session

--- interfaces/plantStore.ts
+++ interfaces/plantStore.ts
@@ -1,5 +1,7 @@
-import create from "zustand";
+import { create } from "zustand";
 import { DatabasePlantType } from "./interfaces";
+import { getUserPlants } from "@/lib/appwrite";
+import { isLoading } from "expo-font";
 
 interface PlantState {
     plants: Record<string, DatabasePlantType>;
@@ -7,7 +9,7 @@ interface PlantState {
     isLoading: boolean;
     error: string | null;
 
-    fetchAllUserPlants: () => Promise<void>;
+    fetchAllUserPlants: (userId: string) => Promise<void>;
     getPlantById: (id: string) => DatabasePlantType | undefined;
     updatePlant: (plant: DatabasePlantType) => Promise<void>;
     deletePlant: (id: string) => Promise<void>;
@@ -20,5 +22,33 @@ export const usePlantStore = create<PlantState>((set, get) => ({
     isLoading: false, 
     error: null,
 
-    fetchAllUserPlants: async () => {}
+    fetchAllUserPlants: async (userId: string) => {
+        set({ isLoading: true, error: null });
+        try {
+            const plantsData = await getUserPlants(userId);
+
+            const plantsById: Record<string, DatabasePlantType> = {};
+            plantsData.forEach(plant => {
+                plantsById[plant.plantId] = plant;
+            });
+
+            set({
+                plants:plantsById,
+                allPlantIds: plantsData.map(plant => plant.plantId),
+                isLoading: false
+            });
+        } catch (err: unknown) {
+            set({isLoading: false, error: err instanceof Error ? err.message : 'An error fetching plants has occured'});
+        }
+    },
+
+    getPlantById: (id: string) => {
+        return get().plants[id];
+    },
+
+    updatePlant: async (plant: DatabasePlantType) => {},
+
+    deletePlant: async (id: string) => {},
+    
+    markAsWatered: async (id: string) => {},
 }))
